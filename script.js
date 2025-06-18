014f61be76ab74fb79172897047cf161e65f77f0
churchofidiots <wolfnamiko@gmail.com>
2025-06-18 04:04:53 +0000

    code lmao

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const form = document.getElementById('name-form');
-const input = document.getElementById('name-input');
-const welcomeMessage = document.getElementById('welcome-message');
+//prerequisites for this shit making sense
+const form = document.getElementById("name-form");
+const input = document.getElementById("name-input");
+const welcomeMessage = document.getElementById("welcome-message");
 
-form.addEventListener('submit', function(event) {
+//the butter to this bread and butter - if/else/else if
+form.addEventListener("submit", function(event) {
     event.preventDefault();
-    
-    const name = input.ariaValueMax;
-    welcomeMessage.textContent = 'welcome, ' + name + '!';
+    const name = input.value.trim();
+
+    if (name === " ") {
+        welcomeMessage.textContent = "Please enter your name!";
+    } else {welcomeMessage.textContent = "Welcome " + name + "chan!";
+    }
 });
+
+//i give my thanks to google, w3 school, and my excruciating note-taking process
